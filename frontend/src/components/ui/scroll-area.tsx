540a59b8532ad9c6c46155c1e12ce785919c9c89
McLanorJEFF <mclanorjephthah@gmail.com>
2026-02-21 19:05:39 +0000

    Initial commit: frontend/src/components/ui/scroll-area.tsx

---
+++ frontend/src/components/ui/scroll-area.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { cn } from "../../lib/utils";
+
+function ScrollArea({
+  className,
+  children,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn("relative overflow-hidden", className)}
+      {...props}
+    >
+      <div className="h-full w-full overflow-auto scrollbar-thin">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export { ScrollArea };
